2a4805024b1b0e775a321cec34b8c84c9a7d0ee4
Wesley Martins <wesmartins96@gmail.com>
2022-05-16 15:05:42 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioBancodeDadosCrud/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace DesafioBancodeDadosCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtLocalizacao = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataSet1 = new System.Data.DataSet();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(461, 190);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(12, 230);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(96, 44);
+            this.btnInserir.TabIndex = 1;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(194, 230);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(96, 44);
+            this.btnAtualizar.TabIndex = 1;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(371, 230);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(102, 44);
+            this.btnDeletar.TabIndex = 1;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(501, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(501, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Localização";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(501, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E-mail";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(504, 64);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(143, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtLocalizacao
+            // 
+            this.txtLocalizacao.Location = new System.Drawing.Point(504, 109);
+            this.txtLocalizacao.Name = "txtLocalizacao";
+            this.txtLocalizacao.Size = new System.Drawing.Size(176, 20);
+            this.txtLocalizacao.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(504, 163);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(176, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(504, 199);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(176, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(650, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(653, 64);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(28, 20);
+            this.txtID.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(693, 291);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLocalizacao);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtLocalizacao;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
+
---
+++ DesafioBancodeDadosCrud/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DesafioBancodeDadosCrud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnAtualizar.Enabled = false;
+            btnDeletar.Enabled = false;
+            
+            string con = @"Data Source=DESKTOP-BVK8T3K\SQLEXPRESS;Initial Catalog=Revisao;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(con);
+
+            sqlCon.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Cliente", sqlCon);
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+
+            da.SelectCommand = cmd;
+
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = ds.Tables[0].TableName;
+
+            sqlCon.Close();
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            string con = @"Data Source=DESKTOP-BVK8T3K\SQLEXPRESS;Initial Catalog=Revisao;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(con);
+
+            string sql = @"INSERT INTO Cliente(Nome, Localizacao, Email)
+                                    VALUES('" + txtNome.Text + "', '" + txtLocalizacao.Text + "', '" + txtEmail.Text + "')";
+
+            SqlCommand cmd = new SqlCommand(sql, sqlCon);
+
+            cmd.CommandType = CommandType.Text;
+            sqlCon.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Inserido com Sucesso!");
+                    LimparCampos();
+                    Form1_Load(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR!" + ex);
+                throw;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtNome.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtLocalizacao.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtEmail.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            string sql = @"UPDATE Cliente SET Nome = '" + txtNome.Text + "', Localizacao = '" + txtLocalizacao.Text + "', Email = '" + txtEmail.Text + "' WHERE ClienteID = '" + txtID.Text + "'";
+            string con = @"Data Source=DESKTOP-BVK8T3K\SQLEXPRESS;Initial Catalog=Revisao;Integrated Security=True";
+
+
+            SqlConnection sqlCon = new SqlConnection(con);
+
+            SqlCommand cmd = new SqlCommand(sql, sqlCon);
+
+            cmd.CommandType = CommandType.Text;
+            sqlCon.Open();
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Atualizado com Sucesso!");
+                    LimparCampos();
+                    Form1_Load(sender, e);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR" + ex);
+                throw;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+
+
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            string sql = @"DELETE FROM Cliente WHERE ClienteID = '" + txtID.Text + "'";
+
+            string con = @"Data Source=DESKTOP-BVK8T3K\SQLEXPRESS;Initial Catalog=Revisao;Integrated Security=True";
+
+            SqlConnection sqlCon = new SqlConnection(con);
+            SqlCommand cmd = new SqlCommand(sql, sqlCon);
+
+            cmd.CommandType = CommandType.Text;
+            sqlCon.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    //const string message = "Deletar este Cadastro?";
+                    //var result = MessageBox.Show(message, "",MessageBoxButtons.YesNo,
+                    //                             MessageBoxIcon.Question);
+                    MessageBox.Show("Deletado com Sucesso");
+                    LimparCampos();
+                    Form1_Load(sender, e);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR!" + ex);
+                throw;
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+        public void LimparCampos()
+        {
+            txtNome.Text = "";
+            txtLocalizacao.Text = "";
+            txtEmail.Text = "";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnDeletar.Enabled = true;
+            btnAtualizar.Enabled = true;
+        }
+    }
+}
